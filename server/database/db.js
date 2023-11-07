46430890c831442771760934f98c0f5067a1d76e
xshreyashiremathx <shreyyhiremath@gmail.com>
2023-11-07 11:20:48 +0530

    intial commit

---
+++ server/database/db.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
+
+const Connection = () => {
+
+    const MONGODB_URI = `mongodb+srv://${USERNAME}:${PASSWORD}@todo.odrvvco.mongodb.net/?retryWrites=true&w=majority`;
+
+    
+
+    mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+    mongoose.connection.on('connected', () => {
+        console.log('Database connected Successfully');
+    })
+
+    mongoose.connection.on('disconnected', () => {
+        console.log('Database disconnected');
+    })
+
+    mongoose.connection.on('error', (err) => {
+        console.log('Error while connecting with the database ', err.message);
+      })
+      
+}
+
+export default Connection;
